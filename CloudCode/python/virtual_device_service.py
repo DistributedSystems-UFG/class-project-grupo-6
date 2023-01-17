cb92d8c910ed19dc7154c057027299340afd1157
Gustavo Marques Reis <62080947+GustavoMarques02@users.noreply.github.com>
2023-01-17 20:00:44 -0300

    Update virtual_device_service.py

--- CloudCode/python/virtual_device_service.py
+++ CloudCode/python/virtual_device_service.py
@@ -12,21 +12,25 @@ import iot_service_pb2_grpc
 # Twin state
 dispositivos = [
     {
+        'id':'tem1',
         'nome':'sensor_temperatura',
         'porta_fisica':None,
         'estado':0
     },
     {
+        'id':'lum1',
         'nome':'sensor_luminosidade',
         'porta_fisica':29,
         'estado':0
     },
     {
+        'id':'led1',
         'nome':'led_vermelho',
         'porta_fisica':16,
         'estado':0
     },
     {
+        'id':'led2',
         'nome':'led_verde',
         'porta_fisica':18,
         'estado':0
@@ -37,39 +41,45 @@ dispositivos = [
 users = [
     {
         'username':'usuario1',
-        'password':'senha1'
+        'password':'senha1',
+        'dispositivos': ['temp1']
     },
     {
         'username':'usuario2',
-        'password':'senha2'
+        'password':'senha2',
+        'dispositivos': ['lum1']
     },
     {
         'username':'usuario3',
-        'password':'senha4'
+        'password':'senha3',
+        'dispositivos': ['led1']
     },
+    {
+        'username':'usuario4',
+        'password':'senha4',
+        'dispositivos': ['lum1','led2']
+    }
 ]
 
 # Kafka consumer to run on a separate thread
 def consume_temperature():
-    sensor = next(disp for disp in dispositivos 
-                    if disp['nome'] == 'sensor_temperatura' 
-                    and disp['porta_fisica'] == None)
+    sensor = next(disp for disp in dispositivos if disp['id'] == 'tem1')
     consumer = KafkaConsumer(bootstrap_servers=KAFKA_SERVER+':'+KAFKA_PORT)
     consumer.subscribe(topics=('temperature'))
     for msg in consumer:
-        print ('Received Temperature: ', msg.value.decode())
-        sensor['estado'] = msg.value.decode()
+        print(msg)
+        #print ('Received Temperature: ', msg.value.decode())
+        #sensor['estado'] = msg.value.decode()
 
 # Kafka consumer to run on a separate thread
 def consume_light_level():
-    sensor = next(disp for disp in dispositivos 
-                    if disp['nome'] == 'sensor_luminosidade' 
-                    and disp['porta_fisica'] == 29)
+    sensor = next(disp for disp in dispositivos if disp['id'] == 'lum1')
     consumer = KafkaConsumer(bootstrap_servers=KAFKA_SERVER+':'+KAFKA_PORT)
     consumer.subscribe(topics=('lightlevel'))
     for msg in consumer:
-        print ('Received Light Level: ', msg.value.decode())
-        sensor['estado'] = msg.value.decode()
+        print(msg)
+        #print ('Received Light Level: ', msg.value.decode())
+        #sensor['estado'] = msg.value.decode()
 
 def produce_led_command(state, ledname):
     producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER+':'+KAFKA_PORT)
@@ -79,9 +89,7 @@ def produce_led_command(state, ledname):
 class IoTServer(iot_service_pb2_grpc.IoTServiceServicer):
 
     def SayTemperature(self, request, context):
-        sensor = next(disp for disp in dispositivos 
-                    if disp['nome'] == 'sensor_temperatura' 
-                    and disp['porta_fisica'] == None)
+        sensor = next(disp for disp in dispositivos if disp['id'] == 'tem1')
         return iot_service_pb2.TemperatureReply(temperature=sensor['estado'])
     
     def BlinkLed(self, request, context):
@@ -90,21 +98,18 @@ class IoTServer(iot_service_pb2_grpc.IoTServiceServicer):
         produce_led_command(request.state, request.ledname)
         # Update led state of twin
         if request.name == 'red':
-            led = next(disp for disp in dispositivos 
-                    if disp['nome'] == 'led_vermelho' 
-                    and disp['porta_fisica'] == 16)
+            led = next(disp for disp in dispositivos if disp['id'] == 'led1')
         else:
-            led = next(disp for disp in dispositivos 
-                    if disp['nome'] == 'led_verde' 
-                    and disp['porta_fisica'] == 18)
+            led = next(disp for disp in dispositivos  if disp['id'] == 'led2')
         led['estado'] = request.state
-        return iot_service_pb2.LedReply(ledstate={'red': dispositivos[2]['estado'], 
-                                                'green':dispositivos[3]['estado']})
+        dat = {
+            'red':dispositivos[2]['estado'],
+            'green':dispositivos[3]['estado']
+        }
+        return iot_service_pb2.LedReply(ledstate=dat)
 
     def SayLightLevel(self, request, context):
-        sensor = next(disp for disp in dispositivos 
-                    if disp['nome'] == 'sensor_luminosidade' 
-                    and disp['porta_fisica'] == 29)
+        sensor = next(disp for disp in dispositivos if disp['id'] == 'lum1')
         return iot_service_pb2.LightLevelReply(lightLevel=sensor['estado'])
 
     def Login(self, request, context):
